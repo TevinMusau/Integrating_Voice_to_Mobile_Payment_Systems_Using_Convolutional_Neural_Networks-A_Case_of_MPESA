fb8b94ed805e3514e12bf9868e2036e46068d16b
TevinMusau <tevinmutua@gmail.com>
2022-09-30 19:41:57 +0300

    Layout Definition

--- app/src/main/java/com/tevin/mvoice/MainActivity.java
+++ app/src/main/java/com/tevin/mvoice/MainActivity.java
@@ -3,6 +3,8 @@ package com.tevin.mvoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void voiceInput(View view)
+    {
+        // Telling us voice input is beginning
+        Toast.makeText(MainActivity.this, "Beginning Voice Input...", Toast.LENGTH_LONG).show();
     }
 }
